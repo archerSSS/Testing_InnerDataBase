9042d7c72fc60bed514ac228769c7df95965b62d
archyrysss <archyrysss@gmail.com>
2021-03-13 14:34:46 +0300

    Добавьте файлы проекта.

---
+++ DataBaseExp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseExp
+{
+    class Program
+    {
+        static SqlConnection connection;
+
+        static void Main(string[] args)
+        {//Integrated Security=SSPI
+            //User Instance=true
+            connection = new SqlConnection(@"data source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;AttachDBFilename=|DataDirectory|Database1.mdf");
+            
+            connection.Open();
+
+            SqlCommand com = new SqlCommand("SELECT * FROM [Table]");
+            com.Connection = connection;
+            SqlDataReader reader = com.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string n = reader.GetString(1);
+                    string s = reader.GetString(2);
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
---
+++ DataBaseExp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DataBaseExp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataBaseExp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("97d86d97-f1ad-40cb-920d-e5ecbf1cdba1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
